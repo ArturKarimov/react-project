6835f4cc49c78d1377dc6d9390b8f3cb549a0449
ArturKarimov <artur.karim@yandex.ru>
2024-02-28 12:16:49 +0300

    Updates

---
+++ serviceWorker.js
@@ -0,0 +1,37 @@
+let CACHE_NAME = "react-burger-pwa";
+const urlsToCache = [
+"/",
+"/index.html",
+];
+self.addEventListener("install", function(event) {
+// Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log("Opened cache");
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(caches.match(event.request)
+        .then(function(response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log("Opened cache");
+                return cache.addAll(urlsToCache);
+            })
+    );
+    self.skipWaiting();
+});
