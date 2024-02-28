04ef4ebc77cf6cce8ceba699a50a68659497c571
ArturKarimov <artur.karim@yandex.ru>
2024-02-28 15:56:08 +0300

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -40,13 +40,30 @@ self.addEventListener('install', e => {
     console.log('ServiceWorker installed');
 });
 
-self.addEventListener("fetch", function(event) {
-    event.respondWith(caches.match(event.request)
-        .then(function(response) {
-            if (response) {
-                return response;
+// self.addEventListener("fetch", function(event) {
+//     event.respondWith(caches.match(event.request)
+//         .then(function(response) {
+//             if (response) {
+//                 return response;
+//             }
+//             return fetch(event.request);
+//         })
+//     );
+// });
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        (async () => {
+            const r = await caches.match(e.request);
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+            if (r) {
+                return r;
             }
-            return fetch(event.request);
-        })
+            const response = await fetch(e.request);
+            const cache = await caches.open(cacheName);
+            console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+            cache.put(e.request, response.clone());
+            return response;
+        })(),
     );
 });
